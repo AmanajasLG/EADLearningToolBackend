341614f1b312182b91f26ee54c04de09427a66e9
Thiago André <thiagoaas1990@gmail.com>
2020-11-30 14:05:49 -0300

    mais uma tentativa

--- api/character/services/character.js
+++ api/character/services/character.js
@@ -11,8 +11,31 @@ module.exports = {
     return strapi.query('character').find(params, p);
   },
 
+
+
   findOne(params, populate) {
+    console.log('findOne called')
     let p = [{path: 'missions', populate: 'questions'}]
     return strapi.query('character').findOne(params, p);
   },
+
+  async update(params, data, { files } = {}) {
+    console.log('update called')
+    const validData = await strapi.entityValidator.validateEntityUpdate(
+      strapi.models.character,
+      data
+    );
+    const entry = await strapi.query('character').update(params, validData);
+
+    if (files) {
+      // automatically uploads the files based on the entry and the model
+      await strapi.entityService.uploadFiles(entry, files, {
+        model: 'character',
+        // if you are using a plugin's model you will have to add the `source` key (source: 'users-permissions')
+      });
+      return this.findOne({ id: entry.id });
+    }
+
+    return entry;
+  }
 };
