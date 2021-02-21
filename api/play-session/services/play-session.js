202ba50160c61738329ac9510865eb94ac4d8668
thiagoa2s <thiagoaas1990@gmail.com>
2021-02-21 17:06:31 -0300

    cria uma playerAction associada a um playSession; deleta o playSession com todos os seus playerAction

--- api/play-session/services/play-session.js
+++ api/play-session/services/play-session.js
@@ -5,4 +5,11 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  //deletes a playSession with all its playerActions
+  async delete(params) {
+    let playSession = await strapi.query('play-session').findOne(params)
+    playSession.player_actions.map(playerAction => strapi.query('player-action').delete({id:playerAction}))
+    return strapi.query('play-session').delete(params);
+  },
+};
--- api/player-action/services/player-action.js
+++ api/player-action/services/player-action.js
@@ -5,4 +5,40 @@
  * to customize this service
  */
 
-module.exports = {};
+const { isDraft } = require('strapi-utils').contentTypes;
+
+module.exports = {
+
+   async create(data, { files } = {}) {
+    const isD = isDraft(data, strapi.models['player-action']);
+
+
+    const validData = await strapi.entityValidator.validateEntityCreation(
+      strapi.models.player_action,
+      data,
+      { isD }
+    );
+
+    const entry = await strapi.query('player-action').create(validData);
+
+    //  checks if a playSession id was passed with charData
+    if(data.play_session){
+      let playSession = await strapi.query('play-session').findOne({id: data.play_session});
+      //  and associates the created register to that playsession
+      if(playSession)
+        await strapi.query('play-session').update({id: playSession.id}, {player_actions: [...playSession.player_actions, entry.id]});
+    }
+
+    if (files) {
+      // automatically uploads the files based on the entry and the model
+      await strapi.entityService.uploadFiles(entry, files, {
+        model: 'player_action',
+        // if you are using a plugin's model you will have to add the `source` key (source: 'users-permissions')
+      });
+      return this.findOne({ id: entry.id });
+    }
+
+    return entry;
+  }
+
+};
