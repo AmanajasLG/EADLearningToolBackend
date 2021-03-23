57930f592aa57af7c91d433a7f4324e3fef7fba2
thiagoa2s <thiagoaas1990@gmail.com>
2021-03-23 12:13:06 -0300

    fix game1 service

--- api/game1missions/services/game1missions.js
+++ api/game1missions/services/game1missions.js
@@ -8,11 +8,11 @@
 module.exports = {
   find(params, populate) {
     let p = [{path: 'locations', populate: 'backgroundAssets'}, {path: 'characters', populate: 'characterAssets'}, 'questions', 'backgroundAudios']
-    return strapi.query('mission').find(params, p);
+    return strapi.query('game1mission').find(params, p);
   },
 
   findOne(params, populate) {
     let p = [{path: 'locations', populate: 'backgroundAssets'}, {path: 'characters', populate: 'characterAssets'}, 'questions', 'backgroundAudios']
-    return strapi.query('mission').findOne(params, p);
+    return strapi.query('game1mission').findOne(params, p);
   },
 };
