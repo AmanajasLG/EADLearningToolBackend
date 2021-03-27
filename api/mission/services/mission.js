f156fbcab0a8056ab0ab5236c7dcb06da5f4657c
AmanajasLG <luizamanajas@hotmail.com>
2021-03-27 13:59:20 -0300

    ATT mission character db

--- api/mission/services/mission.js
+++ api/mission/services/mission.js
@@ -7,12 +7,18 @@
 
 module.exports = {
   find(params, populate) {
-    let p = [{path: 'locations', populate: 'backgroundAssets'}, {path: 'characters', populate: 'characterAssets'}, 'questions', 'backgroundAudios']
+    let p = [{path: 'locations', populate: 'backgroundAssets'}, {path: 'missionCharacters', populate: {
+      path: 'character',
+      populate: 'characterAssets'
+    }}, 'questions', 'backgroundAudios']
     return strapi.query('mission').find(params, p);
   },
 
   findOne(params, populate) {
-    let p = [{path: 'locations', populate: 'backgroundAssets'}, {path: 'characters', populate: 'characterAssets'}, 'questions', 'backgroundAudios']
+    let p = [{path: 'locations', populate: 'backgroundAssets'}, {path: 'missionCharacters', populate: {
+      path: 'character',
+      populate: 'characterAssets'
+    }}, 'questions', 'backgroundAudios']
     return strapi.query('mission').findOne(params, p);
   },
 };
